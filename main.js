b81fbfcf6575240c45f229dcf10408a25a2b3e34
Samraj5413 <samraj6083@gmail.com>
2024-09-28 23:23:36 +0530

    Resolved merge conflicts

--- main.js
+++ main.js
@@ -7,6 +7,25 @@ app.get('/', (req, res) => {
     res.json({ msg: "Hello students!" });
 });
 
+// CRUD functionality
+
+// R - for reading 
+
+app.get('/movies', () => { });
+
+// C - for creating 
+
+app.post('/movies', () => { });
+
+// U - for updating 
+
+app.put('/movies/:id', () => { });
+
+
+// D - for deleting 
+
+app.delete('/movies/:id', () => { });
+
 app.listen(PORT, () => {
     console.log(`The server is running at http://localhost:${PORT}`);
 });
